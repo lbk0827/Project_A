6717ac8cf9a9fb1a69af4c8a2d8c22639a96451b
BK <lbk_0827@naver.com>
2026-05-07 20:10:20 +0900

    feat: Attack 기능 추가

--- project-a/scripts/player/heroine_controller.gd
+++ project-a/scripts/player/heroine_controller.gd
@@ -1,9 +1,24 @@
 extends CharacterBody2D
 
 @export var speed := 180.0
+@export var attack_action: StringName = &"ui_accept"
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
+var is_attacking := false
+
+func _ready():
+	anim.animation_finished.connect(_on_animation_finished)
+	anim.play("Idle")
+
 func _physics_process(_delta):
+	if Input.is_action_just_pressed(attack_action) and not is_attacking:
+		_start_attack()
+
+	if is_attacking:
+		velocity = Vector2.ZERO
+		move_and_slide()
+		return
+
 	var dir := Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
 		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -18,3 +33,12 @@ func _physics_process(_delta):
 		anim.play("Run")
 		if dir.x != 0:
 			anim.flip_h = dir.x < 0
+
+func _start_attack():
+	is_attacking = true
+	velocity = Vector2.ZERO
+	anim.play("Attack")
+
+func _on_animation_finished():
+	if anim.animation == "Attack":
+		is_attacking = false
